5824aca23e769cd81b69da2102da47ff78e3e3e5
Jonathan Doig <z8290362@ad.unsw.edu.au>
2017-02-25 19:13:15 +1100

    New ansible role to create and serve images folder (and later load images)

--- cfdev.sh
+++ cfdev.sh
@@ -1,1 +1,4 @@
-echo ansible-playbook --step geonode_rdf_install.yml --start-at-task=\"Start here\" --extra-vars \"host=cfdev.intersect.org.au\"
+printf '\n'
+printf 'ansible-playbook --step geonode_rdf_install.yml --start-at-task=\"Start here\" --extra-vars \"host=cfdev.intersect.org.au\"\n'
+printf '\n'
+printf 'ansible-playbook load_images.yml --extra-vars \"host=cfdev.intersect.org.au\"\n'
--- help.sh
+++ help.sh
@@ -1,1 +1,4 @@
-echo ansible-playbook --step geonode_rdf_install.yml --start-at-task=\"Start here\" --extra-vars \"host=HOSTNAME\"
+printf '\n'
+printf 'ansible-playbook --step geonode_rdf_install.yml --start-at-task=\"Start here\" --extra-vars \"host=HOSTNAME\"\n'
+printf '\n'
+printf 'ansible-playbook load_images.yml --extra-vars \"host=HOSTNAME\"\n'
