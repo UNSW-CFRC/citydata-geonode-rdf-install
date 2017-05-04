ca431f65c0b22adbfe0b0ec794d71b762cf23969
Jonathan Doig <z8290362@ad.unsw.edu.au>
2017-05-04 14:06:11 +1000

    Update hosts to citydata-*

--- cftest.sh
+++
@@ -1,4 +0,0 @@
-printf '\n'
-printf 'ansible-playbook --step geonode_rdf_install.yml --start-at-task=\"Start here\" --extra-vars \"host=cftest.intersect.org.au\"\n'
-printf '\n'
-printf 'ansible-playbook load_images.yml --extra-vars \"host=cftest.intersect.org.au\"\n'
---
+++ test.sh
@@ -0,0 +1,4 @@
+printf '\n'
+printf 'ansible-playbook --step geonode_rdf_install.yml --start-at-task=\"Start here\" --extra-vars \"host=citydata-test.be.unsw.edu.au\"\n'
+printf '\n'
+printf 'ansible-playbook load_images.yml --extra-vars \"host=citydata-test.be.unsw.edu.au\"\n'
